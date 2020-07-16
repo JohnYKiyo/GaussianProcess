0df7bdd56baddeda2c5f1e1fa4c9b5b18504ad03
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-07-16 14:41:12 +0900

    [add] docstrings

--- gp/GPR.py
+++ gp/GPR.py
@@ -7,7 +7,37 @@ import jax.numpy as np
 from jax import vmap,jit
 
 class GPR(object):
-    def __init__(self, X_train, Y_train, *args, **kwargs):
+    '''Gaussian Process Regression
+    GPR is a nonparametric, Bayesian approach to regression.
+    GPR provides uncertainty measurements on the predictions.
+
+    Attributes:
+        X_train (array-like) :
+            array shape (n_samples, n_features) or (n_samples,)
+            Feature vectors or other representations of training data (also required for prediction).
+        Y_train (array-like) :
+            array shape (n_samples,) or (n_samples, n_targets) )
+            Target values in training data (also required for prediction)
+        kernel:
+    '''
+    def __init__(self, X_train, Y_train,  *args, **kwargs):
+        '''init
+        Args:
+            X_train (array-like) :
+                array shape (n_samples, n_features) or (n_samples,)
+                Feature vectors or other representations of training data (also required for prediction).
+
+            Y_train (array-like) :
+                array shape (n_samples,) or (n_samples, n_targets) )
+                Target values in training data (also required for prediction.
+
+            **alpha (float) :
+                Regularization parameter. Defaults 0.
+
+            **kernel (function or kernel instance) :
+                The kernel used for prediction. The structure of the kernel is the same as the one passed as parameter but with optimized hyperparameters
+                Defaults Gaussian RBF kernel h=0., a=1.
+        '''
 
         self.__X_train = transform_data(X_train)
         self.__Y_train = transform_data(Y_train)
@@ -25,7 +55,24 @@ class GPR(object):
             (self.__alpha+self.__jitter) * np.eye(len(self.__X_train))
         self.__K_inv = np.linalg.inv(self.__K)
 
-    def posterior_predictive(self,x,return_std=False,return_cov=False): 
+    def posterior_predictive(self,x,return_std=False,return_cov=False):
+        '''posterior
+        This methods provides predictions and uncertainties for inputs.
+
+        Args:
+            x (array_like):
+                array shape (n_samples, n_features) or (n_samples,)
+
+            return_std (bool):
+                If true, include Standard deviation of predictions in the output.
+
+            return_cov (bool):
+                If true, include Covariance of predictions in the output.
+        Returns:
+            Value (array-like) : Predictions.
+            Std (array-like) : Standard deviation of predictions each input x if return_std is True.
+            Cov (array-like) : Covariance matrix of predictions if return_cov is True.
+        '''
         X = transform_data(x)
         K_s = self.__kernel(self.__X_train, X)
         K_ss = self.__kernel(X, X)
@@ -47,6 +94,18 @@ class GPR(object):
             return mu_s
 
     def append_data(self,X_train,Y_train):
+        '''Append method
+        Append data to prediction.
+
+        Args:
+            X_train (array-like) :
+                array shape (n_samples, n_features) or (n_samples,)
+                Feature vectors or other representations of training data (also required for prediction).
+
+            Y_train (array-like) :
+                array shape (n_samples,) or (n_samples, n_targets) )
+                Target values in training data (also required for prediction.
+        '''
         X_new = transform_data(X_train)
         Y_new = transform_data(Y_train)
         data_checker(X_new,Y_new)
